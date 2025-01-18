2c98510abad2414b6a55a5edbb0047a0387ac4fc
Aric Rachmany <aricrachmany@gmail.com>
2025-01-18 14:07:32 +0200

    refactor long press directive to support touch event.

--- src/app/directives/long-press.directive.ts
+++ src/app/directives/long-press.directive.ts
@@ -6,22 +6,40 @@ import {Directive, EventEmitter, HostListener, input, Output} from '@angular/cor
 })
 export class LongPressDirective {
 
-  @Output() longPress = new EventEmitter<{mouseEvent: MouseEvent, player: any, team: string}>();
   private timeout: any;
-  data = input<any>();
+  private isPressing = false; // Tracks if the press is active
+  private pressDuration = 2000; // Long press duration in ms
 
+  @Output() longPress = new EventEmitter<{mouseEvent: MouseEvent, player: any, team: string}>();
+  data = input<any>();
   @HostListener('mousedown', ['$event'])
-  onMouseDown(event: MouseEvent): void {
+  @HostListener('touchstart', ['$event'])
+
+  onPressStart(event: MouseEvent | TouchEvent): void {
+    if (this.isPressing) return; // Prevent duplicate triggers
+    this.isPressing = true;
+
     this.timeout = setTimeout(() => {
-      this.longPress.emit({mouseEvent: event, player: this.data().player, team: this.data().team});
-    }, 5000); // Long press duration in ms
+      this.longPress.emit({
+        // @ts-ignore
+        mouseEvent: event,
+        player: this.data()?.player,
+        team: this.data()?.team
+      });
+      this.isPressing = false; // Reset pressing state after emitting
+    }, this.pressDuration);
   }
 
   @HostListener('mouseup')
   @HostListener('mouseleave')
-  onMouseUp(): void {
-    clearTimeout(this.timeout);
+  @HostListener('touchend')
+  @HostListener('touchcancel')
+  onPressEnd(): void {
+    this.clearPress();
   }
-  constructor() { }
 
+  private clearPress(): void {
+    this.isPressing = false;
+    clearTimeout(this.timeout); // Cancel the timeout
+  }
 }
