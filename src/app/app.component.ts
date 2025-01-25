ea249e11e2ae129e8c5549063e38cf446b2a6283
Aric Rachmany <aricrachmany@gmail.com>
2025-01-25 14:33:20 +0200

    added firebase connection. first step refactor code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, signal, ViewChild} from '@angular/core';
+import {Component, computed, ElementRef, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
   CdkDragDrop,
@@ -10,15 +10,22 @@ import {LongPressDirective} from './directives/long-press.directive';
 import {StopwatchComponent} from './stopwatch/stopwatch.component';
 import {ModalComponent} from './modal/modal.component';
 import {GameDetails, GameDetailsComponent} from './game-details/game-details.component';
+import {collection, collectionData, Firestore} from '@angular/fire/firestore';
+import {PlayersService} from './players/players.service';
+import {formatDateToString} from './utils/date-utils';
 
- export interface Player {
+export interface Player {
   name: string;
   rating: number;
-  goals: number;
-  wins: number;
-  loses: number;
-  draws: number;
-  games: number
+  statistics: {
+    [key: string]: { // `key` is used to represent date strings (e.g., 'dd-mm-yyyy')
+      goals: number;
+      wins: number;
+      loses: number;
+      draws: number;
+      games: number;
+    };
+  };
 }
 
  export interface Team {
@@ -27,9 +34,16 @@ import {GameDetails, GameDetailsComponent} from './game-details/game-details.com
   totalRating: number;
 }
 
+export interface GoalModalEvent {
+ team: string;
+ player: Player
+}
+
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, DragDropModule, CommonModule, LongPressDirective, StopwatchComponent, ModalComponent, FormsModule, GameDetailsComponent],
+  imports: [ReactiveFormsModule,
+    DragDropModule, CommonModule, LongPressDirective,
+    StopwatchComponent, ModalComponent, FormsModule, GameDetailsComponent,],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -40,12 +54,15 @@ import {GameDetails, GameDetailsComponent} from './game-details/game-details.com
 // todo: add table for statistics
 // todo: use directive for show hide sensitive data.
 // todo: add permission sensitive data.
+// todo: make players to be signal
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   playerForm!: FormGroup;
   isAdminMode = signal(false);
   originalTeamNames = signal(['teamA', 'teamB', 'teamC']);
+  playersService = inject(PlayersService);
+
 
   teams = {
     teamA: {
@@ -68,7 +85,8 @@ export class AppComponent {
 
   isSetGoalModalVisible = false;
   modalPosition = { x: 0, y: 0 };
-  setGoalModalData!: { team: string; player: Player };
+  setGoalModalData = signal<GoalModalEvent>({} as GoalModalEvent) ;
+  getGoalModalDataByPlayer = computed(() => this.setGoalModalData().player.statistics[this.currentDate].goals || 0)
 
   isTeamWinModalVisible = signal(false);
 
@@ -110,7 +128,15 @@ export class AppComponent {
   addPlayer() {
     const { name, rating } = this.playerForm.value;
     if(name && rating > 0) {
-      this.teams.allPlayers.players.push({ name, rating, goals: 0, wins: 0, loses: 0, draws: 0, games: 0 });
+      this.teams.allPlayers.players.push({ name, rating, statistics: {
+        [this.currentDate]: {
+          goals: 0,
+          wins: 0,
+          loses: 0,
+          games: 0,
+          draws: 0,
+        }
+        } });
       this.playerForm.reset();
       this.nameField.nativeElement.focus();
     }
@@ -145,10 +171,10 @@ export class AppComponent {
     const clientY = event.mouseEvent instanceof MouseEvent ? event.mouseEvent.clientY : (event.mouseEvent as TouchEvent).touches[0].clientY;
 
     this.modalPosition = { x: clientX, y: clientY };
-    this.setGoalModalData = {
+    this.setGoalModalData.set({
       player: event.player,
       team: event.team
-    };
+    });
     this.isSetGoalModalVisible = true;
   }
 
@@ -159,14 +185,14 @@ export class AppComponent {
 
   setGoals(addGoal: boolean) {
     // @ts-ignore
-    const team = this.teams[this.setGoalModalData.team];
-    const playerIndex = team.players.findIndex((player: Player) => player.name === this.setGoalModalData.player.name);
+    const team = this.teams[this.setGoalModalData().team];
+    const playerIndex = team.players.findIndex((player: Player) => player.name === this.setGoalModalData().player.name);
     if(playerIndex >= 0) {
       if(addGoal) {
-        team.players[playerIndex].goals++;
+        team.players[playerIndex].statistics[this.currentDate].goals++;
       } else {
-        if(team.players[playerIndex].goals > 0) {
-          team.players[playerIndex].goals--;
+        if(team.players[playerIndex].statistics[this.currentDate].goals > 0) {
+          team.players[playerIndex].statistics[this.currentDate].goals--;
         }
       }
     }
@@ -186,21 +212,52 @@ export class AppComponent {
     this.isAdminCodeModalVisible.set(false);
   }
 
+  get currentDate() {
+    return formatDateToString(new Date())
+  }
+
   endGame(gameDetails: GameDetails) {
+    const currentDate = this.currentDate // Format the current date as 'dd-mm-yyyy'
+    // Update winners' stats
     // @ts-ignore
-    this.teams[gameDetails.winner].players = this.teams[gameDetails.winner].players.map
-    ((player: Player) =>
-      (gameDetails.gameStatus === 'decided' ?
-        {...player, wins: player.wins+1, games: player.games+1} : {...player, draws: player.draws+1, games: player.games+1})
-    )
+    this.teams[gameDetails.winner].players = this.teams[gameDetails.winner].players.map((player: Player) => {
+      const updatedStatistics = {
+        ...player.statistics,
+        [currentDate]: {...player.statistics[currentDate],
+          wins: player.statistics[currentDate].wins + (gameDetails.gameStatus === 'decided' ? 1 : 0),
+          games: player.statistics[currentDate].games + 1,
+          draws: player.statistics[currentDate].draws + (gameDetails.gameStatus === 'decided' ? 0 : 1)
+        }
+      };
+
+      return {
+        ...player,
+        statistics: updatedStatistics,
+      };
+    });
+
+    // Update losers' stats
     // @ts-ignore
-    this.teams[gameDetails.loser].players = this.teams[gameDetails.loser].players.map
-    ((player: Player) =>
-      (gameDetails.gameStatus === 'decided' ?
-        {...player, loses: player.loses+1, games: player.games+1} : {...player, draws: player.draws+1, games: player.games+1})
-    )
+    this.teams[gameDetails.loser].players = this.teams[gameDetails.loser].players.map((player: Player) => {
+      player.statistics = player.statistics || {};
+      const updatedStatistics = {
+        ...player.statistics,
+        [currentDate]: {...player.statistics[currentDate],
+          loses: player.statistics[currentDate].loses + (gameDetails.gameStatus === 'decided' ? 1 : 0),
+          games: player.statistics[currentDate].games + 1,
+          draws: player.statistics[currentDate].draws + (gameDetails.gameStatus === 'decided' ? 0 : 1)
+        }
+      };
+
+      return {
+        ...player,
+        statistics: updatedStatistics,
+      };
+    });
+
     this.closeTeamWinModal();
   }
+
 
   codeModalSubmitted() {
     this.isAdminCodeModalVisible.set(false);
@@ -209,4 +266,41 @@ export class AppComponent {
     }
     this.code = '';
   }
+
+  ngOnInit(): void {
+    this.playersService.getPlayers().subscribe(val => console.log(val))
+    // this.addPlayerFirebase().then(() => console.log('done'));
+  }
+
+  addPlayerFirebase() {
+    return this.playersService.addPlayer({
+      name: 'yois',
+      rating: 9,
+      statistics: {
+        "2024-01-01": {
+          goals: 9,
+          wins: 5,
+          draws: 9,
+          games: 9
+        },
+        "2024-02-01": {
+          goals: 9,
+          wins: 5,
+          draws: 9,
+          games: 9
+        },
+      }
+    })
+  }
+
+  getPlayerView(player: Player): string {
+    const stats = player.statistics[this.currentDate] ? player.statistics[this.currentDate] : null;
+    if(!stats) {
+      return !this.isAdminMode() ? player.name : player.name + 'rating - ' + player.rating;
+    }
+    return !this.isAdminMode() ?
+      `${player.name} - goals: ${stats.goals} - wins: ${stats.wins} - loses: ${stats.loses} -games: ${stats.games}`
+      : `${player.name} - rating ${player.rating} - goals: ${stats.goals} - wins: ${stats.wins} - loses: ${stats.loses} -games: ${stats.games}`
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {firebaseConfig} from '../../firebase-config';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+ ]
 };
