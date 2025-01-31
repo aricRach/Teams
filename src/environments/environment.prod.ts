9fee08db5bbcb97ff26e63432981f416728fa554
Aric Rachmany <aricrachmany@gmail.com>
2025-01-31 23:50:27 +0200

    test firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,17 +2,16 @@ export const environment = {
   production: true,
   firebase: {
     // @ts-ignore
-    apiKey: `${ANGULAR_FIREBASE_API_KEY}`,
+    apiKey: import.meta.env['NG_APP_FIREBASE_API_KEY'],
     // @ts-ignore
-    authDomain: `${ANGULAR_FIREBASE_AUTH_DOMAIN}`,
+    authDomain: import.meta.env["NG_APP_FIREBASE_AUTH_DOMAIN"],
     // @ts-ignore
-    projectId: `${ANGULAR_FIREBASE_PROJECT_ID}`,
+    projectId: import.meta.env["NG_APP_FIREBASE_PROJECT_ID"],
     // @ts-ignore
-    storageBucket: `${ANGULAR_FIREBASE_STORAGE_BUCKET}`,
+    storageBucket: import.meta.env["NG_APP_FIREBASE_STORAGE_BUCKET"],
     // @ts-ignore
-    messagingSenderId: `${ANGULAR_FIREBASE_MESSAGING_SENDER_ID}`,
+    messagingSenderId: import.meta.env["NG_APP_FIREBASE_MESSAGING_SENDER_ID"],
     // @ts-ignore
-    appId: `${ANGULAR_FIREBASE_APP_ID}`,
-
+    appId: import.meta.env["NG_APP_FIREBASE_APP_ID"]
   }
 };
