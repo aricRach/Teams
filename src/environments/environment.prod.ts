a55d11c06368dec71ad25087c646a0dbe4389163
Aric Rachmany <aricrachmany@gmail.com>
2025-02-01 00:51:12 +0200

    test firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    // @ts-ignore
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-    // @ts-ignore
-    authDomain: process.env["NG_APP_FIREBASE_AUTH_DOMAIN"],
-    // @ts-ignore
-    projectId: process.env["NG_APP_FIREBASE_PROJECT_ID"],
-    // @ts-ignore
-    storageBucket: process.env["NG_APP_FIREBASE_STORAGE_BUCKET"],
-    // @ts-ignore
-    messagingSenderId: process.env["NG_APP_FIREBASE_MESSAGING_SENDER_ID"],
-    // @ts-ignore
-    appId: process.env["NG_APP_FIREBASE_APP_ID"]
+    apiKey: (window as any)['env']?.NG_APP_FIREBASE_API_KEY || '',
+    authDomain: (window as any)['env']?.NG_APP_FIREBASE_AUTH_DOMAIN || '',
+    projectId: (window as any)['env']?.NG_APP_FIREBASE_PROJECT_ID || '',
+    storageBucket: (window as any)['env']?.NG_APP_FIREBASE_STORAGE_BUCKET || '',
+    messagingSenderId: (window as any)['env']?.NG_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+    appId: (window as any)['env']?.NG_APP_FIREBASE_APP_ID || ''
   }
 };
