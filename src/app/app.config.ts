900a66ff306f9a4745a4b8462bbe214a7d1f4576
Aric Rachmany <aricrachmany@gmail.com>
2025-02-04 21:40:35 +0200

    firebase env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import {environment} from '../environments/environment';
 import {DatePipe} from '@angular/common';
-import {firebaseConfig} from '../../firebase-config';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
     DatePipe]
 };
