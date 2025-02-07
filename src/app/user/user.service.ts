0b7a30913b87aa85f3b1a4b41d596969449806fc
Aric Rachmany <aricrachmany@gmail.com>
2025-02-07 15:45:46 +0200

    enable login

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,31 +1,32 @@
-import {Injectable} from '@angular/core';
+import {Injectable, signal} from '@angular/core';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  //
-  // private orgName!: string;
-  // auth = getAuth();
-  // provider = new GoogleAuthProvider();
-  //
-  // user = signal<any>(null)
+
+  private orgName!: string;
+  auth = getAuth();
+  provider = new GoogleAuthProvider();
+
+  user = signal<any>(null)
 
   constructor() { }
-  //
-  // setOrgName(orgName: string) {
-  //   this.orgName = orgName;
-  // }
-  //
-  // googleLogin() {
-  //   signInWithPopup(this.auth, this.provider)
-  //     .then((result) => {
-  //       const user = result.user;
-  //       this.user.set(user);
-  //       console.log("User signed in:", user);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error during Google sign-in:", error.message);
-  //     });
-  // }
+
+  setOrgName(orgName: string) {
+    this.orgName = orgName;
+  }
+
+  googleLogin() {
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        const user = result.user;
+        this.user.set(user);
+        console.log("User signed in:", user);
+      })
+      .catch((error) => {
+        console.error("Error during Google sign-in:", error.message);
+      });
+  }
 }
