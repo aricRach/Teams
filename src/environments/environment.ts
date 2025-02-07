973cb4e2127f118e7c3d609271b630de04a3f1c3
Aric Rachmany <aricrachmany@gmail.com>
2025-02-07 15:26:07 +0200

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,11 @@
   export const environment = {
     production: false,
     firebase: {
-
+      apiKey: (window as any)['env']?.NG_APP_FIREBASE_API_KEY || '',
+      authDomain: (window as any)['env']?.NG_APP_FIREBASE_AUTH_DOMAIN || '',
+      projectId: (window as any)['env']?.NG_APP_FIREBASE_PROJECT_ID || '',
+      storageBucket: (window as any)['env']?.NG_APP_FIREBASE_STORAGE_BUCKET || '',
+      messagingSenderId: (window as any)['env']?.NG_APP_FIREBASE_MESSAGING_SENDER_ID || '',
+      appId: (window as any)['env']?.NG_APP_FIREBASE_APP_ID || ''
     }
   }
