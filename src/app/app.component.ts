9b6ad4903dbbc0562faf7817ad46178f32dac05d
Aric Rachmany <aricrachmany@gmail.com>
2025-02-13 15:43:55 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
@@ -9,6 +9,9 @@ import {RouterOutlet} from '@angular/router';
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    console.log((window as any)['env']?.NG_APP_USER_ADMIN)
+  }
 
 }
