61527e652c4c71e148e1bc185c3ef4521a4deb9c
Aric Rachmany <aricrachmany@gmail.com>
2025-03-11 09:13:55 +0200

    fix infinite spinner. exception selectedGroup undefined.

--- src/app/resolvers/get-group-players.resolver.ts
+++ src/app/resolvers/get-group-players.resolver.ts
@@ -1,4 +1,4 @@
-import { ResolveFn } from '@angular/router';
+import {ResolveFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {PlayersService} from '../players/players.service';
 import {finalize} from 'rxjs';
@@ -8,6 +8,11 @@ export const getGroupPlayersResolver: ResolveFn<any> = (route, state) => {
 
   const playersService = inject(PlayersService);
   const spinnerService = inject(SpinnerService);
+  const router = inject(Router);
+  if(!playersService.selectedGroup()) {
+    router.navigate(['/select-group']);
+    return;
+  }
   spinnerService.setIsLoading(true);
   return playersService.getAllPlayersFromDatabase().pipe(finalize(() => {
     spinnerService.setIsLoading(false);
