af13ca5ebc3aba2965470215468ded383a1416f3
Aric Rachmany <aricrachmany@gmail.com>
2025-03-13 11:23:36 +0200

    audit-trail get all audits of the day by team id even after hard refresh.

--- src/app/audit-trail/services/audit-trail.service.ts
+++ src/app/audit-trail/services/audit-trail.service.ts
@@ -1,6 +1,7 @@
-import {inject, Injectable, signal} from '@angular/core';
+import {computed, inject, Injectable, signal} from '@angular/core';
 import {UserService} from '../../user/user.service';
-import {currentDateTime} from '../../utils/date-utils';
+import {currentDate, currentDateTime} from '../../utils/date-utils';
+import {PlayersService} from '../../players/players.service';
 
 export interface UserAction {
   date: string,
@@ -11,21 +12,28 @@ export interface UserAction {
   providedIn: 'root'
 })
 export class AuditTrailService {
-
   userActions = signal<UserAction[]>([]);
   userService = inject(UserService);
-
-  constructor() { }
+  playersService = inject(PlayersService);
+  localStorageAuditKey = computed(() => `teams-audit-${this.playersService.selectedGroup().id}`)
+  auditTrail = computed(() => {
+    if(this.userActions().length === 0) {
+      const savedAudit = localStorage.getItem(this.localStorageAuditKey());
+      if(savedAudit) {
+        const auditTrail = JSON.parse(savedAudit) as UserAction[];
+        if(auditTrail && auditTrail[0].date.includes(currentDate)) {
+          return auditTrail;
+        }
+      }
+    }
+    return this.userActions();
+  })
 
   addAuditTrail(actionDetails: string) {
-    this.userActions.update((userActions: UserAction[]) => {
-       userActions.unshift({actionDetails: actionDetails, date: currentDateTime(), user: this.userService.user().displayName});
-      console.log(userActions)
-      return userActions;
-    })
-  }
+    const userActions = [...this.auditTrail()];
+    userActions.unshift({actionDetails: actionDetails, date: currentDateTime(), user: this.userService.user().displayName});
+    this.userActions.set(userActions);
+    localStorage.setItem(`teams-audit-${this.playersService.selectedGroup().id}`, JSON.stringify(this.userActions()));
 
-  getAuditTrail() {
-    return [...this.userActions()];
   }
 }
