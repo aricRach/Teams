0cdf910e5bc578da04d24a9c1bbd5e2a8b5892c2
Aric Rachmany <aricrachmany@gmail.com>
2025-04-19 15:04:39 +0300

    Remove firebase config from version control

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,12 @@
   export const environment = {
     production: false,
     firebase: {
-      apiKey: (window as any)['env']?.NG_APP_FIREBASE_API_KEY || '',
-      authDomain: (window as any)['env']?.NG_APP_FIREBASE_AUTH_DOMAIN || '',
-      projectId: (window as any)['env']?.NG_APP_FIREBASE_PROJECT_ID || '',
-      storageBucket: (window as any)['env']?.NG_APP_FIREBASE_STORAGE_BUCKET || '',
-      messagingSenderId: (window as any)['env']?.NG_APP_FIREBASE_MESSAGING_SENDER_ID || '',
-      appId: (window as any)['env']?.NG_APP_FIREBASE_APP_ID || ''
-    }
+      apiKey: "AIzaSyCLqEfAfucHwNRxRQoc1pUeD7K5BmRsZOU",
+      authDomain: "rach-teams.firebaseapp.com",
+      projectId: "rach-teams",
+      storageBucket: "rach-teams.firebasestorage.app",
+      messagingSenderId: "201962993990",
+      appId: "1:201962993990:web:1079d2bd55113d41558d47"
+    },
+    gatewayServiceBaseUrl: 'http://localhost:3002'
   }
